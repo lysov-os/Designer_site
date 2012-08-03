7e802fb41df6824f2075a08db54a036d7a3b5914
Oleg <oleg_lysov@mail.ru>
2012-08-03 20:22:40 +0400

    courasel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 
 class PagesController < ApplicationController
   def home
+  	@header = "Home"
   end
 
   def about
+  	@header = "About"
   end
 
   def portfolio
+  	@header = "Portfolio"
   end
 
   def contact
+  	@header = "Contact"
   end
 end
